b63c6aa0870c7151420c7e26cee9958c5bb30273
Reese Kunz <reesekunz21@gmail.com>
2019-08-06 13:02:45 -0700

    set up fetch, working on fixing undefined now

--- github-user-card/src/Components/UserData.js
+++ github-user-card/src/Components/UserData.js
@@ -1,33 +1,52 @@
 // STEP 1 - Fetch the User Data
-// - When your component mounts, send a GET request to the following URL 
+// - When your component mounts, send a GET request to the following URL
 //   - https://api.github.com/users/reesekunz
 // - After you fetch your data, set it to state
 
-import React from 'react';
+import React from "react";
 
 class UserData extends React.Component {
-constructor() {
-console.log('constructor in UserData.js')
-super();
-this.state = {
-    usergithub: []
-}
-}
+  constructor() {
+    console.log("constructor in UserData.js is running");
+    super();
+    // super gives us acccess to 'this' keyword
+    this.state = {
+      login: null,
+      id: null,
+      following: null
+    };
+  }
 
-componentDidMount(){
-    console.log("component did mount")
-    console.log(this);
+  fetchUserData = () => {
+    fetch("https://api.github.com/users/reesekunz")
+      .then(response => {
+        // first .then is used to format the response/data
+        return response.json();
+        // this becomes the usergithub and is fed back into the next .then
+      })
+      .then(data =>
+        this.setState({
+          login: data.login,
+          id: data.id,
+          following: data.following
+        })
+      )
 
+      .catch(error => {
+        console.log("error", error);
+      });
   };
+
+  componentDidMount() {
+    console.log("component mounted, data = ", this.state.data);
+
+    this.fetchUserData();
+  }
 
   render() {
     console.log("render invoked");
-    return ( 
-
-    )
+    return <div>{this.state.usergithub}</div>;
   }
-};
-
-
+}
 
 export default UserData;
